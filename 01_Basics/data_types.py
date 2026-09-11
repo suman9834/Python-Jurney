5b3e59a83edb375e888a5b65d76b1b7cd393a50b
Suman Kumar <sumankumargin01234@gmail.com>
2026-09-12 02:42:41 +0530

    data type check

--- 01_Basics/data_types.py
+++ 01_Basics/data_types.py
@@ -1,9 +1,10 @@
+name="suman" #string data type
+age=20 #integer data type
+roll = 101 #integer data type
+float_num = 10.5 #float data type
 
-
-
-age= 20
-old = True
-a = None
+boolean_value = True #boolean data type
+none_value = None #None data type
 
 print(type(name))
 print(type(age))
